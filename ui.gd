3532b1e3569bb6452d19fa557909bb836eb1c385
Dexel <DexeloperGames@gmail.com>
2023-02-03 01:20:03 -0500

    crosshair

--- ui.gd
+++ ui.gd
@@ -1,15 +1,14 @@
-extends Node3D
-class_name HitPrimitive
+extends Control
 
-#@export_color_no_alpha var color = Color.WHITE
-@export var mesh : MeshInstance3D
-@export var score_points : int = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("Rotate Y")
+	Engine.max_fps = 60
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+#	$Label.text = str(Performance.TIME_FPS)
+	$Label.text = str(1/delta)
 	pass
